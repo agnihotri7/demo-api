03deca17ff5a746ace4b45261eddd0eedc89b42b
Pradeep Kumar <pradeep.k@atlogys.com>
2022-02-06 00:14:41 +0530

    add account urls to main url file

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -6,4 +6,6 @@ from api.v1 import views
 
 urlpatterns = [
     path('', views.api_root, name='api-root'),
+    # path('account/', include('account.urls')), # ideally all account related urls should follow account/ prefix in url
+    path('', include('account.urls')),
 ]
