99a1dbd0bf890885917271fc79917a34d69f73df
Pradeep Kumar <pradeep.k@atlogys.com>
2022-02-06 00:12:26 +0530

    use new Customer model as auth user

--- config/settings/base.py
+++ config/settings/base.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'account',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +53,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
+
+AUTH_USER_MODEL = "account.Customer"
 
 TEMPLATES = [
     {
